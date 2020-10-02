83ad0a1ec8ccfc9c7b2ae6e883b3e206afdaa515
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-02 17:45:36 +1000

    added all the files and folders need for the website, added the static path for the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import NavBarFull from"./components/NavbarFull/NavbarFull";
+import NavBarFull from "./components/NavbarFull/NavbarFull";
 import Homepage from './components/Homepage/Homepage'
 import Footer from './components/Footer/Footer';
+import Portfolio from './components/Portfolio/Portfolio';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect, useLocation } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
       <NavBarFull />
-      <Homepage />
+
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+        <Switch>
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Switch>
+      </Router>
+
       <Footer />
     </div>
   );
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,5 +3,9 @@ import { } from 'react-bootstrap';
 
 
 const Portfolio = () => {
-    
-}
+    return <h3>Portfolio</h3>
+};
+
+
+
+export default Portfolio;
