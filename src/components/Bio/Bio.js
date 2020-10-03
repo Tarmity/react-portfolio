1c8b2e8067f09ddcfa9e305101062a8ee7bc6200
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-03 18:39:19 +1000

    added and image folder and a cartoon img

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
+import './Img';
 
 
 const Bio = () => {
-    return <h3>Bio</h3>
+    return (
+        <Container>
+        <Card>
+    <Card.Img variant="top" src="./Img/CartoonPic.png" />
+    <Card.Body>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+
+  </Container>
+    )
 };
 
 
