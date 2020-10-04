9dddab583691605550aa1820cf051865adaee7f1
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-05 10:22:10 +1100

    updated my personnal details

--- src/components/CardCont/CardCont.js
+++ src/components/CardCont/CardCont.js
@@ -9,22 +9,42 @@ const CardCont = () => {
 
 
     return (
-        <Media style={{marginLeft: 'auto', marginRight: 'auto', marginTop: '200px', width: '800px', backgroundColor: '#DEB992'}}>
-            <img
-                width={200}
-                height={200}
-                className="mr-3"
-                src={require('./img/HobbyConnect.png')}
-                alt="Generic placeholder"
-            />
-            <Media.Body style={{textAlign: 'center',marginTop: '30px', }}>
-                <h5>Call Me</h5>
-                <p>
-                    <br />
+        <>
+            <Media style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: '200px', width: '800px', backgroundColor: '#DEB992' }}>
+                <img
+                    width={200}
+                    height={200}
+                    className="mr-3"
+                    src={require('./img/HobbyConnect.png')}
+                    alt="Generic placeholder"
+                />
+                <Media.Body style={{ textAlign: 'center', marginTop: '20px', }}>
+                    <h5 style={{fontSize: '50px'}}>Call Me</h5>
+                    <p style={{fontSize: '30px'}}>
+                        <br />
                     +61409298471
                 </p>
-            </Media.Body>
-        </Media>
+                </Media.Body>
+            </Media>
+
+            <Media style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: '200px', width: '800px', backgroundColor: '#DEB992' }}>
+                <img
+                    width={200}
+                    height={200}
+                    className="mr-3"
+                    src={require('./img/HobbyConnect.png')}
+                    alt="Generic placeholder"
+                />
+                <Media.Body style={{ textAlign: 'center', marginTop: '20px',  }}>
+                    <h5 style={{fontSize: '50px'}}>Email Me</h5>
+                    <p style={{fontSize: '30px'}}>
+                        <br />
+                    tarmity.flassman@gmail.com
+                    </p>
+                </Media.Body>
+            </Media>
+        </>
+
 
     )
 }
