8dcba0de26361c5d22aef1ac59764c4c787cc65d
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-06 10:26:03 +1100

    add the props for the json file, ready for the map function

--- src/components/CardCont/CardCont.js
+++ src/components/CardCont/CardCont.js
@@ -16,15 +16,15 @@ const CardCont = (props) => {
                     width={200}
                     height={200}
                     className="mr-3"
-                    src={require('./img/phone.png')}
+                    src= {props.image}
                     alt="Generic placeholder"
                 />
                 <Media.Body style={{ textAlign: 'center', marginTop: '20px', }}>
-                    <h5 style={{fontSize: '50px'}}>Call Me</h5>
+                    <h5 style={{fontSize: '50px'}}>{props.title}</h5>
                     <p style={{fontSize: '30px'}}>
                         <br />
-                    +61409298471
-                </p>
+                    {props.contmeth}
+                    </p>
                 </Media.Body>
             </Media>
 
